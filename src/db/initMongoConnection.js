db64f205a51b48affdfdbe23f591a6b5d0ba6d42
Maria Kasycheva <167572285+Kasycheva@users.noreply.github.com>
2025-01-28 12:48:31 +0100

    Fixed

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -5,13 +5,9 @@ dotenv.config();
 
 export const initMongoConnection = async () => {
   try {
-   
-    const connectionString = `mongodb+srv://MARIA:lVjXs36I2g0S29mI@cluster0.4qnrz.mongodb.net/contacts?retryWrites=true&w=majority&appName=Cluster0`;
+    const connectionString = process.env.MONGODB_URI;
 
-
-    
-    await mongoose.connect(connectionString);
-    
+    await mongoose.connect(connectionString); 
     console.log('Mongo connection successfully established!');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
