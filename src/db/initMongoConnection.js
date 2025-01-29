03316b0ec5318155085dacf92e0a35dd25735607
Maria Kasycheva <167572285+Kasycheva@users.noreply.github.com>
2025-01-29 16:20:38 +0100

    fixed

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -5,12 +5,12 @@ dotenv.config();
 
 export const initMongoConnection = async () => {
   try {
-    const connectionString = process.env.MONGODB_URI;
+    const connectionString = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
 
-    await mongoose.connect(connectionString); 
+    await mongoose.connect(connectionString);
     console.log('Mongo connection successfully established!');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
-    process.exit(1); 
+    process.exit(1);
   }
 };
