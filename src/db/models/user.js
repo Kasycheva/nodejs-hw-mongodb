67bd3bc3a29bdb018cbd69c22b263d8b5399e95a
Maria Kasycheva <167572285+Kasycheva@users.noreply.github.com>
2025-02-23 10:07:46 +0100

    models user

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -12,27 +12,11 @@ const userSchema = new mongoose.Schema(
   { timestamps: true, versionKey: false }
 );
 
+
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
-
-userSchema.pre("findOneAndUpdate", async function (next) {
-  const update = this.getUpdate();
-  if (update.password) {
-    update.password = await bcrypt.hash(update.password, 10);
-  }
-  next();
-});
-
-userSchema.methods.toJSON = function () {
-  const obj = this.toObject();
-  delete obj.password;
-  delete obj.refreshToken;
-  delete obj.sessions;
-  return obj;
-};
 
 userSchema.methods.comparePassword = async function (candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
